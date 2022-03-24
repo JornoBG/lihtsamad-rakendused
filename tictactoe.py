ec4c9f70b2a3ef60a5945ebe4fdd7aaa8da41b4c
JornoBG <jorno.bangusto@gmail.com>
2022-03-24 11:06:48 +0200

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,46 @@
+next_position = input("Please type what position")
+
+if next_position == "1":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "2":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "3":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "4":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "5":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "6":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "7":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "8":
+    print(". x .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "9":
+    print(". x .")
+    print(". . .")
+    print(". . .")
