f371d6b34e2b339d2c9d2548bda0517979ed7493
JornoBG <jorno.bangusto@gmail.com>
2022-03-24 11:28:34 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,46 +1,60 @@
-next_position = input("Please type what position")
+next_position = input("Please type what position: ")
 
 if next_position == "1":
+    print("x . .")
+    print(". . .")
+    print(". . .")
+
+    next_position = input("Please type what position: ")    
+
+    if next_position == "1":
+        print("CANT DO THE MOVE")
+
+
+    elif next_position == "2":
+        print("x o .")
+        print(". . .")
+        print(". . .")
+
+        next_position = input("Please type what position: ")  
+
+        if next_position =="1":
+            print("CANT DO MOVE")
+
+        elif next_position == "2":
+            print("CANT DO THE MOVE")
+
+elif next_position == "3":
+    print("x o x")
+    print(". . .")
+    print(". . .")
+
+elif next_position == "4":
     print(". x .")
     print(". . .")
     print(". . .")
 
-if next_position == "2":
+elif next_position == "5":
     print(". x .")
     print(". . .")
     print(". . .")
 
-if next_position == "3":
+elif next_position == "6":
     print(". x .")
     print(". . .")
     print(". . .")
 
-if next_position == "4":
+elif next_position == "7":
     print(". x .")
     print(". . .")
     print(". . .")
 
-if next_position == "5":
+elif next_position == "8":
     print(". x .")
     print(". . .")
     print(". . .")
 
-if next_position == "6":
-    print(". x .")
-    print(". . .")
-    print(". . .")
-
-if next_position == "7":
-    print(". x .")
-    print(". . .")
-    print(". . .")
-
-if next_position == "8":
-    print(". x .")
-    print(". . .")
-    print(". . .")
-
-if next_position == "9":
+elif next_position == "9":
     print(". x .")
     print(". . .")
     print(". . .")
