279ba7550e81fb9cf58a574a99206575cd339566
JornoBG <jorno.bangusto@gmail.com>
2022-04-05 09:40:23 +0300

    Update LaborinthPL.py

--- LaborinthPL.py
+++ LaborinthPL.py
@@ -1,13 +1,27 @@
-map = [[12, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 24]
+map = [
+    [12, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 24]
 ]
+
+VABA_TEE = 0
+SEIN = 1
 
 start_x = 0
 start_y = 0
 
+def saab_liikuda_paremale(map, praegused_kordinaadid):
+    print("siin toimib see")
+    print(map[0][1] == VABA_TEE)
+    print("siin toimus see")
+    print (praegused_kordinaadid)
+    return False
+
+
+print(saab_liikuda_paremale(map, [start_x, start_y]))
+print(saab_liikuda_paremale(map, [1, 1]))
 def print_kaart():
     print(map[0])
     print(map[1])
@@ -16,4 +30,7 @@ def print_kaart():
     print(map[4])
 
 print_kaart()
+
+
+
 
